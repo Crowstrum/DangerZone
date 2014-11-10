761a4cd0d97074e23e1ecf9d466163cba6d172a5
Koubidoo <xrandapandax@hotmail.com>
2014-11-09 18:23:29 -0600

    Additions to Wall Ability

--- Assets/Scripts/Abilities/WallMove.cs
+++ Assets/Scripts/Abilities/WallMove.cs
@@ -4,13 +4,22 @@ using System.Collections;
 public class WallMove : MonoBehaviour 
 {
 
-    public GameObject player;
+    GameObject player;
+
+    public float amountToRaiseBy = 1.0f;
+
+    void Awake()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
 
 	// Use this for initialization
 	void Start () 
     {
 
-
+        StartCoroutine("MoveWall");
 
 	}
 	
@@ -21,4 +30,23 @@ public class WallMove : MonoBehaviour
 
 	
 	}
+
+    IEnumerator MoveWall()
+    {
+        float finalPosition = player.transform.position.y + 2.0f;
+
+        while (transform.position.y != finalPosition)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y +
+                amountToRaiseBy, transform.position.z);
+
+           // yield return null;
+            yield return new WaitForSeconds(0.5f);
+
+        }
+
+        yield return null;
+
+    }
+
 }
