baffb89e951aeea912729ee1bb435e7275b3dc26
Koubidoo <xrandapandax@hotmail.com>
2014-11-09 18:49:46 -0600

    Changes to Wall Raising
    Wall now raises and lasts for 3 seconds when 'q' is pressed.

--- Assets/Scripts/Abilities/WallMove.cs
+++ Assets/Scripts/Abilities/WallMove.cs
@@ -6,7 +6,9 @@ public class WallMove : MonoBehaviour
 
     GameObject player;
 
-    public float amountToRaiseBy = 1.0f;
+    public float timer = 3.0f;
+
+    public float amountToRaiseBy = 0.0005f;
 
     void Awake()
     {
@@ -27,21 +29,32 @@ public class WallMove : MonoBehaviour
 	void Update () 
     {
 
+        timer -= Time.deltaTime;
 
+        Debug.Log(timer);
+
+        if (timer <= 0)
+        {
+
+            Destroy(gameObject);
+
+            timer = 3.0f;
+
+        }
 	
 	}
 
     IEnumerator MoveWall()
     {
-        float finalPosition = player.transform.position.y + 2.0f;
+        float finalPosition = player.transform.position.y - 0.8f;
 
-        while (transform.position.y != finalPosition)
+        while (transform.position.y < finalPosition)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y +
                 amountToRaiseBy, transform.position.z);
 
            // yield return null;
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.01f);
 
         }
 
