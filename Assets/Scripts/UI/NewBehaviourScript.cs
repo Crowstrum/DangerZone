bb92d7c107462e9cff9f727907a8ec119018d3b0
tylergf <tylerg.fontenot@gmail.com>
2014-11-19 13:18:47 -0600

    menu player selection panel, sounds & sounds on buttons
    added panel for character selection, game sound control now works
    correctly, buttons now have a sound when they are hovered over

---
+++ Assets/Scripts/UI/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonOnClick : MonoBehaviour {
+
+	// Use this for initialization
+	
+	void OnClick()
+	{
+		Application.LoadLevel("test");
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
