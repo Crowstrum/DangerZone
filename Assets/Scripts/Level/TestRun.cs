2dfe9e2846efac27545d02d91cbf3c8bd52f319c
crowstrum <crowstrum@gmail.com>
2014-11-24 13:30:10 -0600

    running

--- Assets/Scripts/Level/TestRun.cs
+++ Assets/Scripts/Level/TestRun.cs
@@ -5,7 +5,12 @@ public class TestRun : MonoBehaviour
 {
 		public LayerMask groundLayer;
 		public GameObject groundObj;
+        public GameObject collisionCheck;
+		public float movespeed = 100f;
+		public float jumpForce = 100f;
 		private bool grounded;
+        private bool wallCollision;
+        private float verticalSpeed;
 		Animator anim;
 		bool facingRight;
 		// Use this for initialization
@@ -14,60 +19,58 @@ public class TestRun : MonoBehaviour
 				anim = GetComponent<Animator> ();
 				facingRight = true;
 		}
+		
+		// Update is called once per frame
 		void FixedUpdate ()
 		{
-				RaycastHit hitInfo;
-				grounded = Physics.Raycast (groundObj.transform.position, Vector3.down, .2f, groundLayer);
+            Debug.Log(rigidbody.velocity.y);
+           
+            if (!grounded)
+            {
+                verticalSpeed = rigidbody.velocity.y;
+                anim.SetFloat("VertSpeed", Mathf.Abs(verticalSpeed));
+            }
+            else if (grounded)
+            {
+                anim.SetFloat("VertSpeed", 0);
+               
+                
+            }
+				grounded = Physics.CheckSphere (groundObj.transform.position, .2f, groundLayer);
+                wallCollision = Physics.CheckSphere(collisionCheck.transform.position, .5f, groundLayer);
 				anim.SetBool ("Grounded", grounded);
 				Debug.DrawLine (groundObj.transform.position, Vector3.down, Color.green);
-       
-
+				
+				float hAxis = Input.GetAxis ("Horizontal");
+                if (!wallCollision)
+                {
+                    anim.SetFloat("Speed", Mathf.Abs(hAxis));
+                    rigidbody.velocity = new Vector3(hAxis * Time.deltaTime * movespeed, rigidbody.velocity.y, 0);
+                }
+				if (hAxis > 0 && !facingRight) {
+						Flip ();
+						facingRight = true;
+				} else if (hAxis < 0 && facingRight) {
+						Flip ();
+						facingRight = false;
+				}
+				
+						
+		
 		}
-		// Update is called once per frame
+		
 		void Update ()
 		{
-				if (Input.GetKeyDown (KeyCode.Space)) {
-						
-						
-						if (grounded) {
-								anim.SetBool ("Jump", true);
-								Vector3 jumpvel = rigidbody.velocity;
-								jumpvel.y = 0;
-								rigidbody.velocity = jumpvel;
-								rigidbody.AddForce (new Vector3 (0, 500f, 0));
-						} 
             
-				} else {
-						anim.SetBool ("Jump", false);
-				}
-				if (Input.GetAxis ("Horizontal") > 0 && grounded) {
-						if (!facingRight) {
-								facingRight = true;
-								Flip ();
-                
-						}
-           
-						anim.SetBool ("isMoving", true);
-						float hAxis = Input.GetAxis ("Horizontal");
-						anim.SetFloat ("Speed", hAxis);
-						rigidbody.velocity = new Vector3 (hAxis * Time.deltaTime * 500f, 0, 0);
-        
-				} else if (Input.GetAxis ("Horizontal") < 0 && grounded) {
-						if (facingRight) {
-								facingRight = false;
-								Flip ();
-                
-						}
-						anim.SetBool ("isMoving", true);
-						float hAxis = Input.GetAxis ("Horizontal");
-						anim.SetFloat ("Speed", hAxis);
-						rigidbody.velocity = new Vector3 (hAxis * Time.deltaTime * 500f, 0, 0);
-
-				} else {
-						anim.SetBool ("isMoving", false);
-						anim.SetFloat ("Speed", 0);
-				}
-	
+				if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			
+								anim.SetBool ("Jump", true);
+								rigidbody.AddForce (new Vector3 (0, jumpForce, 0),ForceMode.Acceleration);
+                }
+                else
+                {
+                    anim.SetBool("Jump", false);
+                }
 		}
 
 		void Flip ()
@@ -75,9 +78,9 @@ public class TestRun : MonoBehaviour
         
 				Quaternion rot = transform.rotation;
 
-                rot = Quaternion.Inverse(rot);
+				rot = Quaternion.Inverse (rot);
               
-                transform.rotation = rot;
+				transform.rotation = rot;
            
         
         
