b610dafe5f5a04113197ee14bd4f4d2c2465a444
Crowstrum <crowstrum@gmail.com>
2014-11-28 19:40:57 -0600

    Death
    all sorts of new death

---
+++ Assets/Scripts/Traps/Belts.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Belts : MonoBehaviour
+{
+		public bool MoveRight = false;
+
+		void OnTriggerStay (Collider col)
+		{
+				switch (MoveRight) {
+				case true:
+						if (col.tag == "Player") {
+								col.rigidbody.AddForce (Vector3.right * 600f);
+						}
+						break;
+				case false:
+						if (col.tag == "Player") {
+								col.rigidbody.AddForce (Vector3.left * 600f);
+						}
+						break;
+				}
+				
+		}
+}
---
+++ Assets/Scripts/Traps/Laser.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour
+{
+
+		public GameObject chargingPart;
+		public GameObject laserPart;
+		
+	
+		public float speed;
+		public bool laserActive;
+	
+		private Vector3 pointA;
+		private Vector3 pointB;
+	
+	
+		IEnumerator Start ()
+		{
+
+		
+				while (true) {
+						yield return StartCoroutine (ChargeUp ());
+						yield return new WaitForSeconds (3.0f);
+						yield return StartCoroutine (FireLaser ());
+						yield return new WaitForSeconds (1.5f);
+						laserActive = false;
+						yield return new WaitForSeconds (3.0f);
+						
+						
+				}
+		}
+	
+		IEnumerator FireLaser ()
+		{
+				Debug.Log ("here");
+
+				float i = 0.0f;
+				float rate = 1.0f / 1.0f;
+				while (i < 1.0f) {
+						i += Time.deltaTime * rate;
+						laserPart.GetComponent<ParticleSystem> ().Play ();
+						laserActive = true;
+						yield return 0; 
+				}
+		}
+
+		IEnumerator ChargeUp ()
+		{
+				float i = 0.0f;
+				float rate = 1.0f / 1.0f;
+				while (i < 1.0f) {
+						i += Time.deltaTime * rate;
+						chargingPart.GetComponent<ParticleSystem> ().Play ();
+						yield return 0; 
+				}
+		}
+
+		void Update ()
+		{
+				Debug.Log (laserActive);
+		}
+}
