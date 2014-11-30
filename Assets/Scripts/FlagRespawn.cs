78eb1bdfd090b4b1052645b09301659551f9796b
Koubidoo <xrandapandax@hotmail.com>
2014-11-30 02:42:22 -0600

    Added Respawning for Flag and Players(incomplete)
    Bleh

---
+++ Assets/Scripts/FlagRespawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlagRespawn : MonoBehaviour 
+{
+
+
+	Vector3 respawnPosition;
+
+	public GameObject flagRespawnPoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		respawnPosition = flagRespawnPoint.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		gameObject.transform.position = respawnPosition;
+
+	}
+}
---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRespawn : MonoBehaviour 
+{
+
+
+	Vector3 playerOneRespawnPosition;
+
+	Vector3 playerTwoRespawnPosition;
+	
+	public GameObject playerOneRespawnPoint;
+
+	public GameObject playerTwoSpawnPoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+		playerOneRespawnPosition = playerOneRespawnPoint.transform.position;
+
+		//playerTwoRespawnPosition = playerTwoSpawnPoint.transform.position;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if(Input.GetKeyDown(KeyCode.Tab))
+		{
+		
+			gameObject.transform.position = playerOneRespawnPosition;
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.T)) 
+		{
+
+			gameObject.transform.position = playerTwoRespawnPosition;
+
+		}
+		
+	}
+}
