f7baf618e3f4e5f17de502447de182402e746701
tylergf <tylerg.fontenot@gmail.com>
2014-11-30 14:37:49 -0600

    item& ability display

--- Assets/Scripts/UI/CharacterDisplay.cs
+++ Assets/Scripts/UI/CharacterDisplay.cs
@@ -19,8 +19,11 @@ public class CharacterDisplay : MonoBehaviour {
 	public Sprite char2;
 	public Sprite char3;
 	public Sprite char4;
+	public Sprite char1A;
+	public Sprite char2A;
+	public Sprite char3A;
+	public Sprite char4A;
 	
-
 //	public Sprite item0;
 //	public Sprite item1;
 //	public Sprite item2;
@@ -30,78 +33,97 @@ public class CharacterDisplay : MonoBehaviour {
 
 	public GameObject leftCharDisplayPort;
 	public GameObject rightCharDisplayPort;
+	public GameObject leftItemDisplayPort;
+	public GameObject rightItemDisplayPort;
+
 
 	UI2DSprite LeftCharPic;
 	UI2DSprite RightCharPic;
+	UI2DSprite LeftItemPic;
+	UI2DSprite RightItemPic;
 
 
 //	GameObject p1_CurrentItem;
 //	GameObject p2_CurrentItem;
+//	public UI2DSprite p1_Item;
+//	public UI2DSprite p2_Item;
 
 
+//	void AbilityOnCoolDown(int inputInt)
+//	{
+//		float coolDownInSeconds = 25f;
+//		float timeStamp = Time.time + coolDownInSeconds;
+//
+//		if(inputInt == 11)
+//		{
+//			// grays out pic
+//			TweenPosition.Begin(leftItemDisplayPort, 1, 0);
+//
+//			//wait some seconds...
+//			if(timeStamp <= Time.time)
+//			//ability comes back on un-grey pic
+//				TweenPosition.Begin(leftItemDisplayPort, 1, 1);
+//		}
+//	}
 
 	void Start () {
 
 		LeftCharPic = leftCharDisplayPort.GetComponent<UI2DSprite> ();
 		RightCharPic = rightCharDisplayPort.GetComponent<UI2DSprite>(); 
+		LeftItemPic = leftItemDisplayPort.GetComponent<UI2DSprite>();
+		RightItemPic = rightItemDisplayPort.GetComponent<UI2DSprite>();
 
 		p1_SelectedChar = CharSelection.p1Char;
 		p2_SelectedChar = CharSelection.p2Char;
 
 		if (p1_SelectedChar == 1) 
 			{
-		
+			LeftItemPic.sprite2D = char1A;
 			LeftCharPic.sprite2D = char1;
 			}
 		if (p1_SelectedChar == 2) 
 			{
-		
-				LeftCharPic.sprite2D = char2;
+			LeftItemPic.sprite2D = char2A;
+			LeftCharPic.sprite2D = char2;
 			}
 
 		if (p1_SelectedChar == 3) 
 			{
-
+			LeftItemPic.sprite2D = char3A;
 			LeftCharPic.sprite2D = char3;
 			}
 		if (p1_SelectedChar == 4) 
 			{
-		
+			LeftItemPic.sprite2D = char4A;
 			LeftCharPic.sprite2D = char4;
 			}
 
 	
 		if (p2_SelectedChar == 1) 
 		{
-
+			RightItemPic.sprite2D = char1A;
 			RightCharPic.sprite2D = char1;
 		}
 		if (p2_SelectedChar == 2) 
 		{
-
+			RightItemPic.sprite2D = char2A;
 			RightCharPic.sprite2D = char2;
 		}
 		
 		if (p2_SelectedChar == 3) 
 		{
-
+			RightItemPic.sprite2D = char3A;
 			RightCharPic.sprite2D = char3;
 		}
-		if (p2_SelectedChar == 4) 
+		if (this.p2_SelectedChar == 4) 
 		{
-
+			RightItemPic.sprite2D = char4A;
 			RightCharPic.sprite2D = char4;
 		}
 
-
-//			if(p2_SelectedChar == 1)
-//				RightCharPic.sprite2D = char1;
-//			if(p2_SelectedChar == 2)
-//				RightCharPic.sprite2D = char2;
-//			if(p2_SelectedChar == 3)
-//				RightCharPic.sprite2D = char3;
-//			if(p2_SelectedChar == 4)
-//				RightCharPic.sprite2D = char4;
+//		p1_Item = p1_CurrentItem.GetComponent<UI2DSprite>();
+//		p2_Item = p2_CurrentItem.GetComponent<UI2DSprite>();
+		
 			
 	}
 	
