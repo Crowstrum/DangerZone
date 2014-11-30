ee0e8b8d93a722b6f1240f04bf10494ede34a7a4
djFaulk <David.Faulk94@gmail.com>
2014-11-30 17:01:22 -0600

    Dodgeball/BowlingBall/Granade update
    Moving across the room, updated items derived classed

--- Assets/Scripts/Level/GrenadeScript.cs
+++ Assets/Scripts/Level/GrenadeScript.cs
@@ -10,7 +10,7 @@ public class GrenadeScript : ItemBase
     // Use this for initialization
     void Awake()
     {
-        expForce = 1000;
+        //expForce = 1000;
     }
 
     // Update is called once per frame
@@ -21,15 +21,15 @@ public class GrenadeScript : ItemBase
         {
             if (this.gameObject.tag == "ThrownItem")
             {
-                Debug.Log("If statement entered");
+                //Debug.Log("If statement entered");
                 yield return new WaitForSeconds(Random.Range(3.0f, 6.0f));
-                Debug.Log("Wait ended");
+                //Debug.Log("Wait ended");
                 onHit();
             }
 
             yield return new WaitForFixedUpdate();
 
-            Debug.Log("Coroutine passed");
+            //Debug.Log("Coroutine passed");
         }
 
     }
@@ -38,7 +38,7 @@ public class GrenadeScript : ItemBase
     {
         Debug.Log("OnHit called");
 
-        //rigidbody.AddExplosionForce(expForce, transform.position, 2.5f);
+        //rigidbody.AddExplosionForce(expForce, transform.position, 5f);
 
         Collider[] hitcolliders = Physics.OverlapSphere(gameObject.transform.position, 4.5f);
 
@@ -46,7 +46,7 @@ public class GrenadeScript : ItemBase
         {
             if (col.gameObject != this.gameObject && col.tag == "Item" || col.tag == "ThrownItem" || col.tag == "Player")
             {
-                Debug.Log("item/player found");
+                //Debug.Log("item/player found");
 
                 Vector3 distance = col.transform.position - gameObject.transform.position;
                 //Vector3 distance = gameObject.transform.position - col.transform.position;
@@ -54,8 +54,8 @@ public class GrenadeScript : ItemBase
                 distance.Normalize();
 
                 //col.rigidbody.velocity = new Vector3(500.0f, 0.0f, 0.0f);
-                col.rigidbody.AddForce(distance * expForce);
-                //col.rigidbody.AddExplosionForce(expForce, transform.position, 2.5f);
+                //col.rigidbody.AddForce(distance * expForce);
+                col.rigidbody.AddExplosionForce(expForce, transform.position, 5f);
 
                 Destroy(gameObject);
             }
